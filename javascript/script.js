de9209f3d88d44300e9e2cbff34a9cc9e44da7a6
Nyagajrr <muchokijnrbenson@gmail.com>
2018-12-03 08:31:25 +0300

    defining variables in js and prompting the user to input values

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+fSide = []
+sSide = []
+tSide = []
+fSide.push(prompt('enter the first side of the triangle '));
+sSide.push(prompt('enter the second side of the triangle '));
+tSide.push(prompt('enter the third side of the triangle '));
