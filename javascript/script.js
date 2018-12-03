e18414bf4072611157bfc7b2ef5642eabb94973c
Nyagajrr <muchokijnrbenson@gmail.com>
2018-12-03 10:54:13 +0300

    adding an okay button on the user interface

--- javascript/script.js
+++ javascript/script.js
@@ -7,17 +7,20 @@ tSide.push(prompt('enter the third side of the triangle '));
 var fstr = fSide.toString();
 var sstr = sSide.toString();
 var tstr = tSide.toString();
+
 if (fstr === sstr && sstr === tstr && fstr  === tstr)
 	{
 	alert('this is an equilateral triangle');
 	}
+else if(fstr+sstr <= tstr || fstr+tstr <= sstr || tstr+fstr <= sstr)
+  {
+  alert('this is not a triangle')
+  }
 else if (fstr === sstr || sstr === tstr || fstr  === tstr)
 	{
 	alert ('this is an isosceles triangle')
 	}
-else if (fstr !== sstr && sstr !== tstr && fstr  !== tstr){
+else if (fstr !== sstr && sstr !== tstr && fstr  !== tstr)
+  {
 	alert('this is an scalene triangle')
-}
-else if(fstr+sstr <= tstr || fstr+tstr <= sstr || tstr+fstr <= sstr){
-alert('this is not a triangle')
-}
+  }
