60381334711bf4a8361009e9c9f1717c5bd6059a
Nyagajrr <muchokijnrbenson@gmail.com>
2018-12-03 08:32:30 +0300

    converting the variables to string

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,6 @@ tSide = []
 fSide.push(prompt('enter the first side of the triangle '));
 sSide.push(prompt('enter the second side of the triangle '));
 tSide.push(prompt('enter the third side of the triangle '));
+var fstr = fSide.toString();
+var sstr = sSide.toString();
+var tstr = tSide.toString();
